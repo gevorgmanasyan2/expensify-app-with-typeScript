df35ece1e30e5e21c5ecef130cc88a132e49775f
gevorgmanasyan2 <gevorgmanasyan2@gmail.com>
2021-05-12 19:21:51 +0400

    two files changes to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
-import AppRouter from "../src/router/AppRouter";
+import AppRouter from "./router/AppRouter";
 import reportWebVitals from "./reportWebVitals";
 import "react-dates/lib/css/_datepicker.css";
 import LoadingPage from "./components/LoadingPage";
